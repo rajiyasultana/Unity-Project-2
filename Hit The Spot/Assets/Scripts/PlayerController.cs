503ae8bb578aed483d27259de04a38788aac9bb3
Rajiya Sultana <rssumu04@gmail.com>
2024-06-26 17:57:29 +0600

    Added power up feature

--- Hit The Spot/Assets/Scripts/PlayerController.cs
+++ Hit The Spot/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private GameObject focalPoint;
+    private float powerUpStrength = 15.0f;  
     public float speed = 15;
     public bool hasPowerUp = false;
     void Start()
@@ -24,6 +26,24 @@ public class PlayerController : MonoBehaviour
         {
             hasPowerUp = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerUp = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerUp)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - transform.position;
+
+            enemyRigidbody.AddForce( awayFromPlayer * powerUpStrength, ForceMode.Impulse );
+            Debug.Log("Collide with : " + collision.gameObject.name + " With powerUp set to: " + hasPowerUp);
         }
     }
 }
